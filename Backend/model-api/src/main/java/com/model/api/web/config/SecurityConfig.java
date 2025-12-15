784c5355cd36a765db4f09a8776a310998d33246
Solmar <solmaruboldi@gmail.com>
2025-12-15 12:32:34 -0300

    Update SecurityConfig with in memory users and roles

--- Backend/model-api/src/main/java/com/model/api/web/config/SecurityConfig.java
+++ Backend/model-api/src/main/java/com/model/api/web/config/SecurityConfig.java
@@ -2,25 +2,63 @@ package com.model.api.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    //Security Filter Chain:
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults()) // Luego permitir origen cruzado en clase: CorsConfig
                 .authorizeHttpRequests(auth -> auth
+                        // Reglas para permitir o negar métodos y usuarios:
+                        .requestMatchers(HttpMethod.GET, "/model/api/**").hasAnyRole("ADMIN", "CUSTOMER")
+                        .requestMatchers(HttpMethod.POST, "/model/api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                        // Reglas para habilitar endpoint para usuarios específicos:
+                        //.requestMatchers("/model/api/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
-
                 )
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
+
+    //In Memory Users:
+    @Bean
+    public UserDetailsService memoryUsers() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails customer = User.builder()
+                .username("customer")
+                .password(passwordEncoder().encode("customer"))
+                .roles("CUSTOMER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, customer);
+    }
+
+    //Password Encoder:
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
