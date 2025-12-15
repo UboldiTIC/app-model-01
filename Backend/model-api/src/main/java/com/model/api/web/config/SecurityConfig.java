596a2977010bb05249270307828887c5a40cfd9c
Solmar <solmaruboldi@gmail.com>
2025-12-15 10:54:10 -0300

    Add CorsConfig

--- Backend/model-api/src/main/java/com/model/api/web/config/SecurityConfig.java
+++ Backend/model-api/src/main/java/com/model/api/web/config/SecurityConfig.java
@@ -13,7 +13,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults()) // Luego permitir origen cruzado en clase: CorsConfig
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
 
